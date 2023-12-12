fa27fd58c74869c6dd4740b837dacd9b417226b4
Aneesha <69849089+Aneesha51@users.noreply.github.com>
2023-12-12 18:45:56 +0200

    Updated the noreferrer pf the app

--- src/App.js
+++ src/App.js
@@ -10,9 +10,8 @@ export default function App() {
      <Weather defaultCity="New York"/>
      <footer>
      <small>
-      <p>This project was by <a href="https://www.linkedin.com/in/aneesha-rangan-41b171196">Aneesha Rangan</a> and is Open-source on <a href="https://github.com/Aneesha51">GitHub</a> and is hosted on  <a href="https://jade-croquembouche-d185e5.netlify.app/">Netlify</a> </p>
-               
-            </small>
+      <p>This project was by <a href="https://www.linkedin.com/in/aneesha-rangan-41b171196" target="_blank" rel="noopener noreferrer">Aneesha Rangan</a> and is Open-source on <a href="https://github.com/Aneesha51" target="_blank" rel="noopener noreferrer">GitHub</a> and is hosted on  <a href="https://jade-croquembouche-d185e5.netlify.app/" target="_blank" rel="noopener noreferrer">Netlify</a> </p>   
+      </small>
      </footer>
     </div>
     </div>
